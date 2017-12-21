58f3f9e1e813d499df94cbb7ee3e7bd55ad31338
Apex-Code <upstaterounder@gmail.com>
2017-12-20 21:27:19 -0600

    update app to display body in JSON

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var app = express();
 var request = require('request');
 const server = 1337;
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
    res.render("home");
 });
 
-app.get("/results", function(){
-   res.render(results.ejs);
+app.get("/results", function(req, res) {
+    request("http://www.omdbapi.com/?s=oklahoma&apikey=808af347", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var parsedData = JSON.parse(body);
+            res.send(parsedData["Search"][0]["Title"]);
+        }
+    });
 });
 
 
